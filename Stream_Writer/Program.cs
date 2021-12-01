8b6dd6d559aae4dc6f1f78583765a72f986597d4
Bruno Carrijo <bruno.carrijo1009@gmail.com>
2021-11-30 21:03:27 -0300

    Codigo usando o StreamWriter

---
+++ Stream_Writer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Stream_Writer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"d:\C#\file1.txt";
+            string targetPath = @"d:\C#\file2.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
